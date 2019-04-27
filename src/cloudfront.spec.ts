84b38b0d775f482773a9a8b1351183298a95164a
nicgirault <nic.girault@gmail.com>
2019-04-27 22:16:35 +0200

    ✨ handle cloudfront distribution creation

--- src/cloudfront.spec.ts
+++ src/cloudfront.spec.ts
@@ -1,13 +1,25 @@
 import { cloudfront } from "./aws-services";
 import { awsResolve } from "./test-helper";
-import { findCloudfrontDistribution, clearCloudfrontCache } from "./cloudfront";
+import {
+  findCloudfrontDistribution,
+  clearCloudfrontCache,
+  identifyingTag,
+  createCloudFrontDistribution
+} from "./cloudfront";
 
 describe("cloudfront", () => {
   describe("findDistribution", () => {
     const listDistributionMock = jest.spyOn(cloudfront, "listDistributions");
+    const listTagsForResourceMock = jest.spyOn(
+      cloudfront,
+      "listTagsForResource"
+    );
+    const waitForMock = jest.spyOn(cloudfront, "waitFor");
 
     afterEach(() => {
       listDistributionMock.mockReset();
+      listTagsForResourceMock.mockReset();
+      waitForMock.mockReset();
     });
 
     it("should return the distribution even if on page 2", async () => {
@@ -37,6 +49,7 @@ describe("cloudfront", () => {
               Items: [
                 {
                   Id: "HELLO",
+                  Status: "Deployed",
                   Origins: {
                     Items: [
                       {
@@ -50,11 +63,53 @@ describe("cloudfront", () => {
           })
         );
 
+      listTagsForResourceMock.mockReturnValue(
+        awsResolve({
+          Tags: {
+            Items: [identifyingTag]
+          }
+        })
+      );
+
       const distribution: any = await findCloudfrontDistribution(
         "hello.example.com"
       );
       expect(distribution).toBeDefined();
       expect(distribution.Id).toEqual("HELLO");
+    });
+
+    it("should for distribution if distribution is not deployed", async () => {
+      listDistributionMock.mockReturnValue(
+        awsResolve({
+          DistributionList: {
+            Items: [
+              {
+                Id: "HELLO",
+                Status: "In Progress",
+                Origins: {
+                  Items: [
+                    {
+                      Id: `S3-Website-hello.example.com.s3-website.eu-west-3.amazonaws.com`
+                    }
+                  ]
+                }
+              }
+            ]
+          }
+        })
+      );
+
+      listTagsForResourceMock.mockReturnValue(
+        awsResolve({
+          Tags: {
+            Items: [identifyingTag]
+          }
+        })
+      );
+      waitForMock.mockReturnValue(awsResolve());
+
+      await findCloudfrontDistribution("hello.example.com");
+      expect(waitForMock).toHaveBeenCalledTimes(1);
     });
   });
 
@@ -74,6 +129,45 @@ describe("cloudfront", () => {
       expect(invalidationParams.DistributionId).toEqual("some-distribution-id");
       expect(invalidationParams.InvalidationBatch.Paths.Items[0]).toEqual(
         "/index.html"
+      );
+    });
+  });
+
+  describe("createCloudFrontDistribution", () => {
+    const createDistributionWithTagsMock = jest.spyOn(
+      cloudfront,
+      "createDistributionWithTags"
+    );
+
+    afterEach(() => {
+      createDistributionWithTagsMock.mockReset();
+    });
+
+    it("should create a distribution and wait for it to be available", async () => {
+      const distribution = {};
+      createDistributionWithTagsMock.mockReturnValue(
+        awsResolve({ Distribution: distribution })
+      );
+      const result = await createCloudFrontDistribution(
+        "hello.lalilo.com",
+        "arn:certificate"
+      );
+      expect(result).toBe(distribution);
+      expect(createDistributionWithTagsMock).toHaveBeenCalledTimes(1);
+      const distributionParam: any =
+        createDistributionWithTagsMock.mock.calls[0][0];
+      const distributionConfig =
+        distributionParam.DistributionConfigWithTags.DistributionConfig;
+      expect(distributionConfig.Origins.Items[0].DomainName).toEqual(
+        "hello.lalilo.com.s3-website.eu-west-3.amazonaws.com"
+      );
+      expect(
+        distributionConfig.DefaultCacheBehavior.ViewerProtocolPolicy
+      ).toEqual("redirect-to-https");
+      expect(distributionConfig.DefaultCacheBehavior.MinTTL).toEqual(0);
+      expect(distributionConfig.DefaultCacheBehavior.Compress).toEqual(true);
+      expect(distributionConfig.ViewerCertificate.ACMCertificateArn).toEqual(
+        "arn:certificate"
       );
     });
   });
--- src/cloudfront.ts
+++ src/cloudfront.ts
@@ -1,7 +1,11 @@
 import { cloudfront, bucketRegion, websiteEndpoint } from "./aws-services";
 import { getAll } from "./aws-helper";
 import { logger } from "./logger";
-import { DistributionSummary } from "aws-sdk/clients/cloudfront";
+import {
+  DistributionSummary,
+  DistributionConfig,
+  Tag
+} from "aws-sdk/clients/cloudfront";
 
 export const findCloudfrontDistribution = async (originBucketName: string) => {
   const distributions = await getAll<DistributionSummary>(
@@ -27,17 +31,139 @@ export const findCloudfrontDistribution = async (originBucketName: string) => {
 
   for (const distribution of distributions) {
     if (
-      distribution.Origins.Items[0] &&
-      distribution.Origins.Items[0].Id === getOriginId(originBucketName)
+      !distribution.Origins.Items[0] ||
+      distribution.Origins.Items[0].Id !== getOriginId(originBucketName)
     ) {
-      logger.info(`cloudfront distribution found: ${distribution.Id}`);
-      return distribution;
+      continue;
     }
+
+    logger.info(`cloudfront distribution found: ${distribution.Id}`);
+
+    const { Tags } = await cloudfront
+      .listTagsForResource({ Resource: distribution.ARN })
+      .promise();
+
+    if (
+      !Tags ||
+      !Tags.Items ||
+      !Tags.Items.find(
+        tag =>
+          tag.Key === identifyingTag.Key && tag.Value === identifyingTag.Value
+      )
+    ) {
+      throw new Error(
+        `Distribution "${
+          distribution.Id
+        }" does not seem to have been created by aws-spa. You should delete it...`
+      );
+    }
+    if (distribution.Status === "In Progress") {
+      await cloudfront
+        .waitFor("distributionDeployed", { Id: distribution.Id })
+        .promise();
+    }
+    return distribution;
   }
 };
 
+export const createCloudFrontDistribution = async (
+  domainName: string,
+  sslCertificateARN: string
+) => {
+  const distributionConfig = getDistributionConfig(
+    domainName,
+    sslCertificateARN
+  );
+
+  logger.info(
+    `[CloudFront] Creating Cloudfront distribution with bucket website origin...`
+  );
+  const { Distribution } = await cloudfront
+    .createDistributionWithTags({
+      DistributionConfigWithTags: {
+        DistributionConfig: distributionConfig,
+        Tags: {
+          Items: [identifyingTag]
+        }
+      }
+    })
+    .promise();
+
+  if (!Distribution) {
+    throw new Error("Could not create cloudfront distribution");
+  }
+
+  // logger.info(
+  //   `[CloudFront] Waiting for distribution to be available. This step might takes up to 25 minutes...`
+  // );
+  // await cloudfront
+  //   .waitFor("distributionDeployed", { Id: Distribution.Id })
+  //   .promise();
+  return Distribution;
+};
+
+const getDistributionConfig = (
+  domainName: string,
+  sslCertificateARN: string
+): DistributionConfig => ({
+  CallerReference: Date.now().toString(),
+  Aliases: {
+    Quantity: 1,
+    Items: [domainName]
+  },
+  Origins: {
+    Quantity: 1,
+    Items: [
+      {
+        Id: getOriginId(domainName),
+        DomainName: getS3DomainName(domainName),
+        CustomOriginConfig: {
+          HTTPPort: 80,
+          HTTPSPort: 443,
+          OriginProtocolPolicy: "http-only"
+        }
+      }
+    ]
+  },
+  Enabled: true,
+  Comment: "",
+  DefaultCacheBehavior: {
+    ViewerProtocolPolicy: "redirect-to-https",
+    TargetOriginId: getOriginId(domainName),
+    ForwardedValues: {
+      QueryString: false,
+      Cookies: {
+        Forward: "none"
+      }
+    },
+    AllowedMethods: {
+      Quantity: 2,
+      Items: ["HEAD", "GET"],
+      CachedMethods: {
+        Quantity: 2,
+        Items: ["HEAD", "GET"]
+      }
+    },
+    TrustedSigners: {
+      Enabled: false,
+      Quantity: 0
+    },
+    MinTTL: 0,
+    Compress: true // this is required to deliver gzip data
+  },
+  ViewerCertificate: {
+    ACMCertificateArn: sslCertificateARN,
+    SSLSupportMethod: "sni-only",
+    MinimumProtocolVersion: "TLSv1.1_2016",
+    CertificateSource: "acm"
+  }
+});
+
+const getS3DomainName = (domainName: string) =>
+  `${domainName}.${websiteEndpoint[bucketRegion]}`;
+
 const getOriginId = (domainName: string) =>
-  `S3-Website-${domainName}.${websiteEndpoint[bucketRegion]}`;
+  `S3-Website-${getS3DomainName(domainName)}`;
 
 export const clearCloudfrontCache = async (distributionId: string) => {
   logger.info("Creating CloudFront invalidation...");
@@ -62,4 +188,9 @@ export const clearCloudfrontCache = async (distributionId: string) => {
   //   DistributionId: distributionId,
   //   Id: Invalidation.Id
   // });
+};
+
+export const identifyingTag: Tag = {
+  Key: "created-by",
+  Value: "aws-spa"
 };
